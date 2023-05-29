0f2cb5b2595ace37388302cc1338a3b6972f7b6a
Medjou Mehdi <medjou.mehdi@hotmail.com>
2023-05-29 17:55:50 +0200

    Finish routes equipements and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
+
+  resources :equipments do
+    resources :bookings, only: [:create]
+  end
+
+  resources :bookings, only: [:destroy, :edit, :update]
+
+  # resources :categories do
+  #   resources :equipments
+
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
