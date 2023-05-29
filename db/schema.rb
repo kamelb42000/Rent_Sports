03a03a5cea92128a1ccf8a9a2970317a3037df2a
Medjou Mehdi <medjou.mehdi@hotmail.com>
2023-05-29 15:19:30 +0200

    fix db users

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_123959) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_125805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
     t.boolean "accepted"
     t.date "date"
-    t.bigint "user_id", null: false
-    t.bigint "equipment_id", null: false
+    t.bigint "users_id", null: false
+    t.bigint "equipments_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["equipment_id"], name: "index_bookings_on_equipment_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["equipments_id"], name: "index_bookings_on_equipments_id"
+    t.index ["users_id"], name: "index_bookings_on_users_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -32,30 +32,33 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_123959) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "equipment", force: :cascade do |t|
+  create_table "equipments", force: :cascade do |t|
     t.string "name"
     t.string "year"
     t.integer "price"
     t.text "description"
-    t.bigint "user_id", null: false
-    t.bigint "categorie_id", null: false
+    t.bigint "users_id", null: false
+    t.bigint "categories_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["categorie_id"], name: "index_equipment_on_categorie_id"
-    t.index ["user_id"], name: "index_equipment_on_user_id"
+    t.index ["categories_id"], name: "index_equipments_on_categories_id"
+    t.index ["users_id"], name: "index_equipments_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "bookings", "equipment"
-  add_foreign_key "bookings", "users"
-  add_foreign_key "equipment", "categories", column: "categorie_id"
-  add_foreign_key "equipment", "users"
+  add_foreign_key "bookings", "equipments", column: "equipments_id"
+  add_foreign_key "bookings", "users", column: "users_id"
+  add_foreign_key "equipments", "categories", column: "categories_id"
+  add_foreign_key "equipments", "users", column: "users_id"
 end
