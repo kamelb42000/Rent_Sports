b8972c83119653d4d0fffa8babb047aea220817e
Billal Medjou <medjoubillal@gmail.com>
2023-06-01 11:34:40 +0200

    finish booking

--- app/models/stuff.rb
+++ app/models/stuff.rb
@@ -2,4 +2,7 @@ class Stuff < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :bookings, dependent: :destroy
+  def accepted?
+    bookings.exists?(accepted: true)
+  end
 end
