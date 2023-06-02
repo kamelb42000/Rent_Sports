65dbc0efab132bbc55f109afa9a907428a84e6cf
kamel besseghier <bbs42000@hotmail.fr>
2023-06-02 14:54:06 +0200

    update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,3 +7,6 @@ import { application } from "./controllers/application"
 
 import NavbarController from "./controllers/navbar_controller.js"
 application.register("navbar", NavbarController)
+
+
+// document.querySelector("body > div.d-flex > div.w-50.col-lg-4 > div > div > div.mapboxgl-control-container > div.mapboxgl-ctrl-bottom-right > div > div").remove();
